c5f6506e567ac08f4f618d3a3ae7863bc980b2c4
Kyle Brown <56103801+KyleBrown-804@users.noreply.github.com>
2020-08-20 17:04:31 -0700

    Recipe Object adds to Recipes Table
    -Added firebase call to add Recipe object to Recipes table when a user selects and image or takes a picture

--- Back-End/Database/RecipeQueries.js
+++ Back-End/Database/RecipeQueries.js
@@ -1,10 +1,21 @@
 import firebase from "../../FirebaseConfig";
 import { db } from "./FireDBConfig";
+import { getUserID } from "./User";
 
 export async function addRecipe(Recipe) {
   if (Recipe != null || Recipe != undefined) {
     console.log("adding Recipe...");
     console.log(Recipe);
+
+    db.collection("Recipes")
+      .doc()
+      .set(Recipe)
+      .then(() => {
+        console.log("Recipe added to the database!");
+      })
+      .catch((error) => {
+        console.log("Error occured trying to add recipe to the db: " + error);
+      });
   } else {
     console.log("Error: Recipe returned null or undefined");
   }
